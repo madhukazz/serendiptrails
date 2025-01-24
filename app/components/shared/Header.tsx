d8c129ec0b3cb4aeec43a7079c9af13118017505
madhukazz <madhukadilshan8@gmail.com>
2025-01-25 00:43:14 +0530

    Implement Header and Navbar components with responsive design and navigation items

--- app/components/shared/Header.tsx
+++ app/components/shared/Header.tsx
@@ -1,33 +1,24 @@
 import Image from "next/image"
 import Logo from "@/public/assets/images/logo.png"
-import Link from "next/link";
-
-interface NavItem {
-    name: string;
-    href: string;
-}
-
-const NavItems: NavItem[] = [
-    { name: "Home", href: "/" },
-    { name: "Tours", href: "/" },
-    { name: "Destinations", href: "/" },
-    { name: "Gallery", href: "/" },
-    { name: "Contact Us", href: "/" },
-];
+import Navbar from "./Navbar"
 
 const Header = () => {
-  return (
-    <header className="bg-white py-2">
-        <div className="w">
-            <Image className="w-[250px]" src={Logo} alt="Serendiptrails Logo" />
-            <ul>
-                {NavItems.map((item: NavItem, index: number) => (
-                    <li key={index}><Link href={item.href}>{item.name}</Link></li>
-                ))}
-            </ul>
-        </div>
-    </header>
-  )
+    return (
+        <header className="bg-white py-2 shadow-md">
+            <div className="w flex items-center justify-between">
+                <Image className="w-[230px]" src={Logo} alt="Serendiptrails Logo" />
+                <Navbar />
+                <div className="flex gap-5">
+                    <button className="bg-white rounded-full p-2 w-12 h-12 shadow-sm group hover:bg-primary">
+                        <i className="text-primary text-xl ri-dashboard-line group-hover:text-white transition-all duration-300"></i>
+                    </button>
+                    <button className="bg-white rounded-full p-2 w-12 h-12 shadow-sm group hover:bg-primary">
+                        <i className="text-primary text-xl ri-search-2-line group-hover:text-white transition-all duration-300"></i>
+                    </button>
+                </div>
+            </div>
+        </header>
+    )
 }
 
 export default Header
---
+++ app/components/shared/Navbar.tsx
@@ -0,0 +1,63 @@
+'use client'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface NavItem {
+    name: string;
+    href: string;
+}
+
+const NavItems: NavItem[] = [
+    { name: "Home", href: "/" },
+    { name: "Tours", href: "/" },
+    { name: "Destinations", href: "/" },
+    { name: "Gallery", href: "/" },
+    { name: "Contact Us", href: "/" },
+];
+
+const tourItems: NavItem[] = [
+    { name: "Adventure Sports", href: "/adventure-sports-tours" },
+    { name: "Honeymoon Packages", href: "/honeymoon-tours" },
+    { name: "Multiday Tours", href: "/multiday-tours" },
+    { name: "Day Out Packages", href: "/day-out-packages" },
+    { name: "Digital Nomad", href: "/digital-nomad" },
+    { name: "Custom Tours", href: "/custom-tours" },
+];
+
+const Navbar = () => {
+
+    const path: string = usePathname();
+
+  return (
+        <ul className="flex gap-6">
+            {NavItems.map((item: NavItem, index: number) => (
+                <li className="font-montserrat relative group" key={index}>
+                    <Link
+                        className={`text-secondary font-semibold uppercase text-sm hover:text-primary transition-all duration-300 group ${
+                            path === item.href ? "text-primary" : ""
+                        }`}
+                        href={item.href}
+                    >
+                        {item.name}
+                        {item.name === "Tours" && (
+                            <i className="ri-add-line ml-1 text-primary transform transition-transform duration-300 group-hover:rotate-45"></i>
+                        )}
+                    </Link>
+
+                    {/* Tours submenu */}
+                    {item.name === "Tours" && (
+                        <ul className="absolute left-0 invisible group-hover:visible bg-white shadow-lg mt-2 space-y-2 w-[250px]">
+                            {tourItems.map((tour: NavItem, index: number) => (
+                                <li key={index} className="font-montserrat hover:bg-primary hover:text-white transition-all duration-300 py-2 px-3 !mt-0">
+                                    <Link className="text-sm font-medium inline-block w-full" href={tour.href}>{tour.name}</Link>
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </li>
+            ))}
+        </ul>
+  )
+}
+
+export default Navbar
