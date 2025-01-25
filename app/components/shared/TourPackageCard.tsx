34409e63d4e716c88802abd3e4aeec1a93b37499
madhukazz <madhukadilshan8@gmail.com>
2025-01-25 19:02:17 +0530

    Update TourPackageCard component; add rounded corners to the duration badge for improved aesthetics

--- app/components/shared/TourPackageCard.tsx
+++ app/components/shared/TourPackageCard.tsx
@@ -7,7 +7,7 @@ const TourPackageCard = () => {
     <div className="rounded-xl bg-white shadow-lg group">
         <div className="relative">
             <Image className="rounded-t-xl" src={TourPackageImage} alt="tour image" />
-            <span className="bg-primary text-white absolute top-5 left-5 p-1 w-24 text-center text-sm font-jost font-semibold"><i className="ri-time-line font-normal mr-1"></i>13 Nights</span>
+            <span className="bg-primary text-white absolute top-5 left-5 p-1 w-24 text-center text-sm font-jost font-semibold rounded-md"><i className="ri-time-line font-normal mr-1"></i>13 Nights</span>
         </div>
         <div className="p-5 flex flex-col gap-4">
             <h3 className="font-semibold text-xl">Ultimate Sri Lanka Multiday Tour – 14 Days</h3>
