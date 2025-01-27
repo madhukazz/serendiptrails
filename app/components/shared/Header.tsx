d94e53705b3cfb31e76acaee1c40be296a318372
madhukazz <madhukadilshan8@gmail.com>
2025-01-27 22:27:37 +0530

    Refactor Header component: remove commented code and maintain scroll detection threshold at 50px for improved performance

--- app/components/shared/Header.tsx
+++ app/components/shared/Header.tsx
@@ -1,46 +1,3 @@
-// 'use client'
-// import Image from "next/image"
-// import Logo from "@/public/assets/images/logo.png"
-// import Navbar from "./Navbar"
-// import { useEffect, useState } from "react";
-
-// const Header = () => {
-
-//     const [isScrolled, setIsScrolled] = useState(false);
-
-//     useEffect(() => {
-//         const handleScroll = () => {
-//           setIsScrolled(window.scrollY > 150);
-//         };
-    
-//         window.addEventListener("scroll", handleScroll);
-    
-//         return () => {
-//           window.removeEventListener("scroll", handleScroll);
-//         };
-//       }, []);
-
-//     return (
-//         <header className={`bg-white py-2 shadow-lg z-10 transition-all duration-700 ${isScrolled ? "fixed transform translate-y-0 w-[-webkit-fill-available] shadow-xl transition-all duration-700" : ""}`}>
-//             <div className="w flex items-center justify-between">
-//                 <Image className="w-[230px]" src={Logo} alt="Serendiptrails Logo" />
-//                 <Navbar />
-//                 <div className="flex gap-5">
-//                     <button className="bg-white rounded-full p-2 w-12 h-12 shadow-sm group hover:bg-primary">
-//                         <i className="text-primary text-xl ri-dashboard-line group-hover:text-white transition-all duration-300"></i>
-//                     </button>
-//                     <button className="bg-white rounded-full p-2 w-12 h-12 shadow-sm group hover:bg-primary">
-//                         <i className="text-primary text-xl ri-search-2-line group-hover:text-white transition-all duration-300"></i>
-//                     </button>
-//                 </div>
-//             </div>
-//         </header>
-//     )
-// }
-
-// export default Header
-
-
 'use client';
 import Image from "next/image";
 import Logo from "@/public/assets/images/logo.png";
@@ -52,7 +9,7 @@ const Header = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      setIsScrolled(window.scrollY > 50); // Trigger change after scrolling 50px
+      setIsScrolled(window.scrollY > 50);
     };
 
     window.addEventListener("scroll", handleScroll);
